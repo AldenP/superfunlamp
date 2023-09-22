cc42aef1a3c086aeb03bdc8024592f84243a89bb
juliampacheco <142916635+juliampacheco@users.noreply.github.com>
2023-09-22 17:43:06 -0400

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,37 +1,78 @@
 <?php
-include 'util.php';
 
-	$request = getRequest();
-    
-	// $username = $request[usernameKey];
-	// $password = $request[passwordKey];
-	// $firstName = $request[firstNameKey];
-	// $lastName = $request[lastNameKey];
-	$username = $request -> username;
-	$password = $request -> password;
-	$firstName = $request -> firstName;
-	$lastName = $request -> lastName;
+        $inData = getRequest();
 
-	$database = "UserDatabase"; // ini_get("database")
-	$connection = new mysqli("localhost", "lisa", "saxophone", "ContactManager");
-	// $connection->select_db($database);
-	$err = $connection->connect_error;
-        if ($err) 
+        $firstName = $inData["firstName"];
+        $lastName = $inData["lastName"];
+        $username = $inData["username"];
+        $password = $inData["passWord"];
+        $existingPassword = "";
+
+        $database = "UserDatabase"; // ini_get("database")
+        $connection = new mysqli("localhost", "lisa", "saxophone", "ContactManager");
+        // $connection->select_db($database);
+        $err = $connection->connect_error;
+        if ($err)
         {
-	returnError($err);
-	//return;
+        returnError($err);
+        //return;
         }
-	
-	$stmt = $connection->prepare("INSERT into Users (FirstName,LastName,Username,Password) VALUES(?,?,?,?)");
-	$stmt->bind_param("ssss", $firstName, $lastName, $username, $password);
-	$stmt->execute();
-	if (mysqli_stmt_errno($stmt) != 0)
-	{
-		$err = $stmt->$error;
-		returnError($err);
-		//return;
-	}
-	$stmt->close();
-	$connection->close();
+        elseif ($firstName == "" ||  $username == "" ||  $password == "")
+        {
+                returnError("Please fill in the required field");
+        }
+
+        elseif (userExists($conn, $username, $existingPassword))
+        {
+                http_response_code(409);
+                returnError("Username already exists with password: $existingPassword");
+        }
+        else
+        {
+                $stmt = $conn->prepare("INSERT into Users (firstName,lastName,username,passWord) VALUES(?,?,?,?)");
+                $stmt->bind_param("ssss", $firstName, $lastName, $username, $password);
+                $stmt->execute();
+                $stmt->close();
+                $conn->close();
+
+                http_response_code(200);
+                returnError("");
+        }
+
+
+        function getRequest()
+        {
+                return json_decode(file_get_contents('php://input'), true);
+        }
+
+        function sendResult( $obj )
+        {
+                header('Content-type: application/json');
+                echo $obj;
+        }
+
+        function returnError( $err )
+        {
+                $retValue = '{"error":"' . $err . '"}';
+                sendResult( $retValue );
+        }
+
+        function userExists($conn, $username, &$existingPass)
+        {
+                $stmt = $conn->prepare("select * from Users where username like ?");
+                $stmt->bind_param("s", $username);
+                $stmt->execute();
+
+                $result = $stmt->get_result();
+
+                if ($row = $result->fetch_assoc())
+                {
+                        $existingPass = $row["Password"];
+                        return true;
+                }
+
+                return false;
+        }
+
 
 ?>
