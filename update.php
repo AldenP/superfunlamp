929a2edd03ea89a66f88b57a03f2fb17e399c38a
Alina <93550993+anguyen644@users.noreply.github.com>
2023-09-22 12:34:36 -0400

    Updated file

--- update.php
+++ update.php
@@ -3,21 +3,30 @@ include 'util.php';
 
 $request = getRequest();
 
-$userID = $request[$userIDKey];
-$id = $request[$idKey];
-$phoneNumber = $request[$phoneNumberKey];
-$email = $request[$emailKey];
-$firstName = $request[$firstNameKey];
-$lastName = $request[$lastNameKey];
+// $userID = $request[userIDKey];
+// $id = $request[idKey];
+// $phoneNumber = $request[phoneNumberKey];
+// $email = $request[emailKey];
+// $firstName = $request[firstNameKey];
+// $lastName = $request[lastNameKey];
+$userID = $request -> userID;
+$id = $request -> id;
+$phoneNumber = $request -> phoneNumber;
+$email = $request -> email;
+$firstName = $request -> firstName;
+$lastName = $request -> lastName;
 
-$connection = new mysqli();
-$connection->select_db(ini_get("database"));
-if ($connection->connect_error) {
-	returnWithError($connection->connect_error);
+$database = "UserDatabase"; // ini_get("database")
+$connection = new mysqli("insert server", "insert user", "insert password", $database, 3306);
+// $connection->select_db($database);
+$err = $connection->connect_error;
+if ($err) 
+{
+	returnError($err);
 	return;
 }
 
-$stmt = $connection->prepare("UPDATE Contacts PhoneNumber = ?, Email = ?, FirstName = ?, LastName = ?) WHERE UserID = ? AND ID = ?");
+$stmt = $connection->prepare("UPDATE Contacts SET PhoneNumber = ?, Email = ?, FirstName = ?, LastName = ? WHERE UserID = ? AND ID = ?");
 $stmt->bind_param("ssssii", $phoneNumber, $email, $firstName, $lastName, $userID, $id);
 $stmt->execute();
 $stmt->close();
