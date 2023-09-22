bae821fb636a0cab8bc88c6b08ece71980302738
Alina <93550993+anguyen644@users.noreply.github.com>
2023-09-22 12:35:44 -0400

    Fixed connection until apache is added

--- create.php
+++ create.php
@@ -3,21 +3,30 @@ include 'util.php';
 
 $request = getRequest();
 
-$userID = $request[$userIDKey];
-$phoneNumber = $request[$phoneNumberKey];
-$email = $request[$emailKey];
-$firstName = $request[$firstNameKey];
-$lastName = $request[$lastNameKey];
+// $userID = $request[userIDKey];
+// $phoneNumber = $request[phoneNumberKey];
+// $email =  $request[emailKey];
+// $firstName =  $request[firstNameKey];
+// $lastName = $request[lastNameKey];
+$userID = $request -> userID;
+$phoneNumber = $request -> phoneNumber;
+$email =  $request -> email;
+$firstName =  $request -> firstName;
+$lastName = $request -> lastName;
 
-$connection = new mysqli();
-$connection->select_db(ini_get("database"));
-if ($connection->connect_error) {
-	returnWithError($connection->connect_error);
+$database = "UserDatabase"; // ini_get("database")
+$connection = new mysqli("insert server", "insert user", "insert password", $database, 3306);
+// $connection->select_db($database);
+$err = $connection->connect_error;
+if ($err) 
+{
+	returnError($err);
 	return;
 }
 
 $stmt = $connection->prepare("INSERT into Contacts (UserId,PhoneNumber,Email,FirstName,LastName) VALUES(?,?,?,?,?)");
-$stmt->bind_param("issss", $userId, $phoneNumber, $email, $firstName, $lastName);
+
+$stmt->bind_param("issss", $userID, $phoneNumber, $email, $firstName, $lastName);
 $stmt->execute();
 $stmt->close();
 $connection->close();
